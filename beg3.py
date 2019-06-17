ab4a93ca5015339f6ef2406b3a2a5737f974f748
vishnuprakasa6 <51432099+vishnuprakasa6@users.noreply.github.com>
2019-06-17 12:38:56 +0530

    Create beg3.py

---
+++ beg3.py
@@ -0,0 +1,12 @@
+    
+#include <stdio.h>
+int main()
+{
+ch char;
+scanf("%c",&ch);
+if(char=='a'||char=='e'||char=='i'||ch=='o'||char=='u'||char=='A'||char=='E'||char=='I'||char=='O'||char=='U')
+printf("Vowel");
+else
+printf("Consonant");
+return 0;
+}
